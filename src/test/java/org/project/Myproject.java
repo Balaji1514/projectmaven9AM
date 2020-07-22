ff0b1cdd737dd411eb07180834f41cc109d0c161
paiyaabala8 <paiyaabala8@gmail.com>
2020-07-22 08:30:38 +0530

    update

--- src/test/java/org/project/Myproject.java
+++ src/test/java/org/project/Myproject.java
@@ -1,7 +1,10 @@
 package org.project;
 
 public class Myproject {
-
+public void merge(String a, String b ) {
+ String c = a.concat(b);
+ System.out.println(c); 
+}
 	public static void main(String[] args) {
 		String s1 = "Greens ";
         String s2 = "greens";
@@ -16,6 +19,11 @@ public class Myproject {
         System.out.println(x);
 		String substring = s3.substring(7, 13);
 		System.out.println(substring);
+		Myproject m =new Myproject();
+		m.merge("name", "name");
+		
+		char c = s1.charAt(3);
+		System.out.println(c);
 	}
 
 }
